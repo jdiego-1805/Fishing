9859ab636e53b3e055bcb041aed5f82341714f53
Milywayz <jtjenks@gmail.com>
2023-08-23 11:54:38 -0500

    done with footer for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,36 +3,42 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer
-      class="bg-white dark:bg-gray-900"
-      style={{ backgroundColor: "#2F4550" }}
+      class="bg-white dark:bg-gray-900 text-lg"
+      style={{ backgroundColor: "#2F4550", fontSize: 20 }}
     >
       <div class="mx-auto w-full max-w-screen-xl">
-        <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-          <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              <Link className="text-gray-900" to="/">
-                <h1>Company</h1>
-              </Link>
-            </h2>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              <Link className="text-gray-900" to="/Lodging">
-                <h1>Lodging</h1>
-              </Link>
-            </h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="#" class=" hover:underline">
-                  About
-                </a>
-              </li>
-            </ul>
+        <div class="md:justify-between">
+          <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
             <div>
-              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Contact
+              <h2 class="mb-8 text-lg font-semibold text-blue-900 uppercase dark:text-white">
+                <Link className="text-blue-400" to="/">
+                  <h1 className="hover:underline">Company</h1>
+                </Link>
               </h2>
-              <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                  <h1>254-266-0543</h1>
+            </div>
+            <div>
+              <h2 class="mb-6 text-lg font-semibold text-gray-900 uppercase dark:text-white">
+                <Link className="text-blue-400" to="/Lodging">
+                  <h1 className="hover:underline">Lodging</h1>
+                </Link>
+              </h2>
+            </div>
+            <div>
+              <h2 class="mb-6 text-lg font-semibold text-gray-900 uppercase dark:text-white">
+                <Link className="text-blue-400" to="/Photos">
+                  <h1 className="hover:underline">Photos</h1>
+                </Link>
+              </h2>
+            </div>
+            <div>
+              <h2 class="mb-6 text-lg font-semibold text-gray-900 uppercase dark:text-white">
+                <Link className="text-blue-400" to="/Photos">
+                  <h1>Contact</h1>
+                </Link>
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <h1 className="text-gray-900">254-266-0543</h1>
                 </li>
               </ul>
             </div>
